7ff5f0916ece343dea30442102356ad10b66ee17
Om <emailtoop@gmail.com>
2023-08-14 16:39:46 -0700

    chap2 report (list dict, tuple)

--- Work/02_chap.py
+++ Work/02_chap.py
@@ -1,23 +1,23 @@
 '''
-# Working with Data
+# Chap 2: Working with Data
 https://dabeaz-course.github.io/practical-python/Notes/02_Working_with_data/00_Overview.html
 '''
-##### 2.1 Datatypes and Data structures
+        ##### 2.1 Datatypes and Data structures
 
 ## none type: None is often used as a placeholder for optional or missing value. It evaluates as False in conditionals.
 email_addr = None
 
-## Tuples - collection of values grouped together. represent simple records or structures.
-## A good analogy: A tuple is like a single row in a database table.
-## Contents are ordered but can not be modified
+    ## Tuples - collection of values grouped together. represent simple records or structures.
+    ## A good analogy: A tuple is like a single row in a database table.
+    ## Contents are ordered but can not be modified. It is read only
 
 s = ('GOOG', 100,440.23)
 s1 = 'YAHO',88,212.12   # is also tuple type and bracket is optional
 s2 = ()     # an empty tuple
 print(s[2])
 
-# Tuple packing -- like swift programming lang; related items are packed together in a single entity.
-# this is then passed to function or returned from function.
+    # Tuple packing -- like swift programming lang; related items are packed together in a single entity.
+    # this is then passed to function or returned from function.
 s = ('Id',22,'F')   # id, age and gender can be packed together
 print(s)
 
@@ -25,8 +25,8 @@ print(s)
 id,age,gender = s   
 print(id,age,gender)
 
-## Dictionaries { } -- key value mapping
-
+    ## Dictionaries { } -- key value mapping
+    ##
 s = { 'id':'Id11',
       'age':22,
       'Gender': 'F'
@@ -34,7 +34,7 @@ s = { 'id':'Id11',
 
 print(s['id'])
 
-# excercise
+# excercise using tuple, reading csv file
 import os
 import csv
 
@@ -42,8 +42,75 @@ fpath = os.getcwd()
 fpath += r'\work\data\portfolio.csv'
 with open(fpath,'rt') as f:
     rows =csv.reader(f)
-    z =next(rows)
+    hdr =next(rows)
+    print(hdr)
     r = next(rows)
     print(r)
     (a,b,c)  = next(rows)
-    
+    print(a,b,c)
+    for row in rows:
+        (a,b,c) = row
+        print((a,b,c))        
+        print("---" + row[0],row[1])
+
+# iterate throught dict items
+dict1 = s
+items = dict1.items()
+print(items)
+for i in dict1:
+    print(i, dict1[i])
+
+
+'''
+2.2 Containers
+==============
+Lists. Ordered data.
+Dictionaries. Unordered data.
+Sets. Unordered collection of unique items.
+'''
+# list of tuples
+portfolio = [
+    ('GOOG', 100, 490.1),
+    ('IBM', 50, 91.3),
+    ('CAT', 150, 83.44)
+]
+# list of dictionaries
+portfolio2 = [
+    {'name': 'GOOG', 'shares' :100, 'price':490.1},
+    {'name': 'IBM', 'shares' :50, 'price':90.1},
+    {'name': 'CAT', 'shares' :150, 'price':49.1},    
+]
+x1 = []
+x1.append("one")
+len(x1)
+print(x1)
+#dict
+
+priceList = []  # list 
+priceDict = {}  #dict
+def prices_ex():
+    import os
+    import csv
+    fpath = os.getcwd()
+    fpath += r'\work\data\prices.csv'
+    with open(fpath,'rt') as f:        
+        for line in f:
+            xm = line.split(',')
+            if len(xm) >=2:            
+                priceList.append([xm[0],float(xm[1])])                
+                priceDict[xm[0]] = float(xm[1])        
+    print(priceList)
+    print(priceDict)    
+prices_ex()
+for d in priceDict:
+    print(d,priceDict.get(d))
+
+print(priceDict.get('IBM',0.0))
+
+## Sets  {} -Sets are collection of unordered ---unique--- items.
+## Sets are useful for membership tests.
+## Sets are also useful for duplicate elimination.
+tech_books = {'abc', 'def', 'ghi','ghi'}
+tech_stock = set([123,423,121])
+tech_stock
+123 in tech_stock
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,129 @@
+'''
+https://dabeaz-course.github.io/practical-python/Notes/02_Working_with_data/02_Containers.html
+https://gto76.github.io/python-cheatsheet/#dictionary
 # report.py
-#
-# Exercise 2.4
+
+
+# Exercise 2.4 A list of tuples
+define a function read_portfolio(filename) that opens a given portfolio file and reads it into a list of tuples.
+'''
+def read_portfolio(fname):
+    dataTup =()
+    dataList = []
+    dataDict = {}
+    try:        
+        with open(fname, 'rt') as f:
+            hdr = f.readline()  #ignore hdr
+            for line in f:
+                #print (line)
+                dataTup = line.split(',')
+                if len(dataTup) > 2:                    
+                    dataList.append(tuple([dataTup[0], int(dataTup[1]), float(dataTup[2])]))
+    except FileExistsError:
+        print('fiile not found')
+    return dataList
+
+## Exercise 2.5: List of Dictionaries
+## reading portfolio.csv 
+def read_portfolio2(fname):
+    dataTup =()
+    dataList = []
+    dataDict = {}
+    try:        
+        with open(fname, 'rt') as f:
+            hdr = f.readline()  #ignore hdr
+            temp = hdr.split(',')            
+            for line in f:
+                #print (line)
+                dataTup = line.split(',')
+                if len(dataTup) > 2:
+                    #dataList.append(dataTup[0], int(dataTup[1]), float(dataTup[2]))                    
+                    dataList.append(dict({'name':dataTup[0], 'shares':dataTup[1], 'price':dataTup[2]}))                                        
+    except FileExistsError:
+        print('fiile not found')
+    return dataList
+
+## calling methods above
+import os
+import csv
+
+workDir = os.getcwd()
+portFile = workDir + r'/work/data/portfolio.csv'
+priceFile =workDir + r'/work/data/prices.csv'
+mylist1 = read_portfolio(portFile)
+mylist2 = read_portfolio2(portFile)
+print(mylist1)
+print("-------------------------------")
+print(mylist2)
+
+## try ...
+tDict = {}
+i =0
+for k in mylist2:   # read each dict from the list
+    k.keys()
+    k.values()
+    k.items()
+    k.get('name') 
+    xd = dict(zip(k.keys(),k.values()))
+    tDict[i] = xd
+    i +=1
+
+print(tDict)
+
+
+## Exercise 2.6: Dictionaries as a container
+## Write a function read_prices(filename) that reads a set of prices such as this into a 
+## dictionary where the keys of the dictionary are the stock names and the values in the dictionary are the stock prices.
+
+def read_prices(fname):
+    import csv
+    dict1 ={}
+    try:
+        with open(fname,'rt') as f:
+            rows = csv.reader(f)
+            #hdr = next(rows)
+            #print(hdr)
+            for row in rows:
+                print(row)
+                if(len(row) >= 2):
+                    dict1[row[0]] = row[1]
+    except FileNotFoundError:
+        print('File not found -- {}',fname)
+    return dict1
+
+dt = read_prices(priceFile)
+print(dt)
+
+'''
+##Exercise 2.7: Finding out if you can retire
+Tie all of this work together by adding a few additional statements to your report.py program that computes gain/loss.
+These statements should take the list of stocks in Exercise 2.5 and the dictionary of prices in Exercise 2.6 and compute 
+the current value of the portfolio along with the gain/loss.
+'''
+
+def gain_loss(portfolioFile,pricesFile):
+    pol = read_portfolio(portfolioFile)
+    prd = read_prices(pricesFile)
+    gain_loss = []
+    for tu in pol:      # for each tuple in the list        
+        strx = str(tu[0]).replace("\"","")
+        if strx in prd.keys():
+            val = prd.get(strx)
+            pur_price = int(tu[1]) * float(tu[2])
+            cur_price = int(tu[1]) * float(val)
+            if pur_price < cur_price:
+                st = 'gain'
+            else:        
+                st = 'loss'
+            val = cur_price - pur_price
+            tux = (tu[0],tu[1],tu[2], val,st)            
+            gain_loss.append(tux)
+    # total loss/gain print
+    tot =0
+    for a in gain_loss:
+        #tot = tot + float(a[3])
+        print(a)
+    print(f'Total loss/gain = {tot:0.4f}')    
+
+gain_loss(portFile,priceFile)
+
