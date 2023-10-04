f947f0150c3f88575f024758476102b43733a458
Om <emailtoop@gmail.com>
2023-10-03 21:17:27 -0700

    update

--- Work/02_chap.py
+++ Work/02_chap.py
@@ -3,11 +3,12 @@
 https://dabeaz-course.github.io/practical-python/Notes/02_Working_with_data/00_Overview.html
 '''
         ##### 2.1 Datatypes and Data structures
+        #####
 
 ## none type: None is often used as a placeholder for optional or missing value. It evaluates as False in conditionals.
 email_addr = None
 
-    ## Tuples - collection of values grouped together. represent simple records or structures.
+    ## Tuples   ()- collection of values grouped together. represent simple records or structures.
     ## A good analogy: A tuple is like a single row in a database table.
     ## Contents are ordered but can not be modified. It is read only
 
@@ -62,7 +63,7 @@ for i in dict1:
 
 
 '''
-2.2 Containers
+######  2.2 Containers
 ==============
 Lists. Ordered data.
 Dictionaries. Unordered data.
@@ -114,3 +115,214 @@ tech_books = {'abc', 'def', 'ghi','ghi'}
 tech_stock = set([123,423,121])
 tech_stock
 123 in tech_stock
+
+'''
+                #######  2.3 Formatting
+f-string -- The part {expression:format} is replaced.
+Format codes (after the : inside the {}) are similar to C printf(). Common codes include:
+d       Decimal integer
+b       Binary integer
+x       Hexadecimal integer
+f       Float as [-]m.dddddd
+e       Float as [-]m.dddddde+-xx
+g       Float, but selective use of E notation
+s       String
+c       Character (from integer)
+'''
+name = 'ibm'
+shares = 100
+price =91.11
+f'{name:>10s}{shares:>10d} {price:>10.2f}'
+
+s = {       #dict
+    'name': 'IBM',
+    'shares': 100,
+    'price': 91.1
+}
+'{name:>10s} {shares:10d} {price:10.2f}'.format_map(s)
+## using format method
+
+## C-Style formatting
+print('price of apple in fruticana is %d cent' %99)     ##note there is no ' ,' and there is % to fetch value
+print('price of %s in fruticana is %f %s' %('apple', 99.9, 'cent'))
+
+## f format doc https://docs.python.org/3/library/string.html#format-specification-mini-language
+value = 4323.1004
+print(f'{value:0.4f}')
+print(f'{value:2.2f}')
+print(f'{value:12.2f}')
+print(f'{value:>12.2f}')
+print(f'{value:<12.2f}')
+print(f'{value:$^12.2f}')
+print(f'{value:$>12.2f}')
+
+'''
+2.4 Sequences: Python has three Seq data types - 
+    String  ''
+    List    []
+    Tuple   ()
+ > All sequences are ordered, indexed by integers, and have a length.
+ > Sequences can be replicated: s * n.
+ > Sequences of the same type can be concatenated: s + t.
+ > Slicing of a sequence - s[start:end]
+'''
+
+## Exercise 2.13: Counting
+### range(): looping over sequence 
+for i in range(10,20):
+    print(i)
+
+for n in range(10,0,-1):       # Count 10 ... 1
+        print(n, end=' ')
+
+for i in range(0,10,1):       # Count 10 ... 1
+        print(i, end=' ')
+
+### enumerating over sequence
+names = ['aa','bx','dfds','zee']
+for i,val in enumerate(names):
+    print(i,val)
+
+for i in enumerate(names):
+    print(i)
+    type(i)
+    lst=list(i)
+    print(lst, type(lst))
+    tu = tuple(i)
+    print(tu, type(tu))
+    dct = {}
+    dct[i[0]] = i[1]
+    print(dct, type(dct))
+
+### zip() function
+# combines multiple sequences and makes an iterator that combines them
+names = ['aa','bx','dfds','zee']
+price = [22.22,43.53,6436.77,45.34]
+
+shares = zip(names,price)                   # cobines and results in iterator
+shares_list = list(zip(names,price))        #converts zip list iterator to list
+for col,val in shares:
+    print(col,val)
+
+# Exercise 2.15: A practical enumerate() example
+'''
+Recall that the file Data/missing.csv contains data for a stock portfolio, but has some rows with missing data. 
+Using enumerate(), modify your pcost.py program so that it prints a line number with the warning message when it 
+encounters bad input.
+'''
+import os
+import csv
+workdir = os.getcwd()
+workdir += r'\work\data'
+with open(workdir+r'\missing.csv','rt') as f:
+     rows = csv.reader(f)
+     for i,row in enumerate(rows):
+          try:
+            for val in row:
+                 if len(val) <= 0:
+                    raise ValueError                                
+            print(i, ' ', row, ' ')
+          except ValueError:
+               print(f'## Row {i}: Bad row:{row}')
+
+##Exercise 2.16: Using the zip() function
+'''
+'''
+import os
+import csv
+workdir = os.getcwd()
+workdir += r'\work\data'
+holding =[]
+zholding=[]
+hdr = ['name','shares', 'price','current price', 'purchase cost', 'current cost','gain/loss']
+with open(workdir +r'\portfolio.csv','rt') as f, open(workdir +r'\prices.csv','rt') as g:
+    rows = csv.reader(f)
+    header = next(rows)    
+    for i,row in enumerate(rows):                
+        print(f'i={i} " " {row}')
+        g.seek(0)
+        prices = csv.reader(g)
+        for item in prices:
+           # print(item)
+            #strx = item.split(',')
+           # print("strx =", strx)
+            if row[0] == item[0]:
+                #print('******match', row[0] ,item[0])                
+                status = row                
+                cur_price = float(item[1])
+                pur_cost = float(row[1])* float(row[2])
+                cur_cost = float(row[1])* float(cur_price)
+                gain_loss = cur_cost - pur_cost
+                temp = [cur_price,pur_cost,cur_cost,gain_loss]
+                status += temp
+                print('--> in')
+                zst = list(zip(hdr, status))
+                zholding.append(zst)
+                holding.append(status)
+                break
+    
+
+print("#------------Report -----------------#\n")
+print(hdr)
+for i, share in enumerate(holding):    
+        print( f'{i} {share}\n')
+
+print("#------------Report2 -----------------#\n")
+print(hdr)
+for i, share in enumerate(zholding):    
+        print( f'{i} {share}\n')
+
+
+## Exercise 2.17: Inverting a dictionary using zip()
+##Note-- When used in comparisons, tuples are compared element-by-element starting with the first item.
+##    -- zip() is often used in situations like this where you need to pair up data from different places. 
+##      -- Also, be aware that zip() stops once the shortest input sequence is exhausted.
+
+prices = {
+        'GO':55,
+        'MS':33,
+        'fb':66
+}
+
+# to get list of value key
+price_list = list(zip(prices.values(),prices.keys()))
+price_list
+v,k = price_list[0]
+print(sorted(price_list))
+a = [1, 2, 3, 4]
+b = ['w', 'x', 'y', 'z']
+c = [0.2, 0.4, 0.6]
+list(zip(a, b, c))
+
+
+'''##                             ##'''
+##        2.5 collections module    ##
+'''##                             ##'''
+
+# tabulate total shares of each stock
+
+portfolio = [
+    ('GOOG', 100, 490.1),
+    ('GOOG', 120, 390.1),
+    ('IBM', 50, 91.3),
+    ('CAT', 150, 83.44),
+    ('GE', 150, 83.44),
+    ('GE', 100, 76.44),
+    ('GOOG', 67, 290.1)
+]
+
+# use counter() object from collections module
+from collections import Counter
+total_shares = Counter()
+for name,shares,price in portfolio:
+    total_shares[name] +=shares
+
+total_shares['GE']
+
+# using defaultdict
+
+
+'''##                             ##'''
+##       2.6 List Comprehensions    ##
+'''##                             ##'''
+
